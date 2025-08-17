a4bd50d4bba1397cc58bb89137bd7272ffb40c5a
unafec <unafecrdc@gmail.com>
2025-08-17 14:28:21 +0100

    J'ajoute les fichiers sur le site UNAFEC
    J'ajoute les fichiers

---
+++ script.js
@@ -0,0 +1,3 @@
+// C'est ici que vous ajouterez des fonctionnalités interactives si nécessaire.
+// Par exemple, un menu de navigation responsive pour les appareils mobiles.
+// Pour l'instant, ce fichier est vide mais il est important qu'il soit lié.
